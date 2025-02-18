d58b4101d568eb669a42a5b92bb956568ce9d8f5
Athan <jonpuray@gmail.com>
2025-02-19 01:56:07 +0800

    created main

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+
+
+if __name__ == '__main__':
+    print('test')
+
+    scopes = [
+    "https://www.googleapis.com/auth/spreadsheets"
+    ]
+
+    creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+    client = gspread.authorize(creds)
+
+    sheet_id = "1ZmilDNuV_h-w1OkKNwlbZCyD42KpaL5ilEK1hELRJpo"
+    sheet = client.open_by_key(sheet_id)
+
+    values_list = sheet.sheet1.row_values(1)
+    st.write(values_list)
