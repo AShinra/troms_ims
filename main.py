30b65c5b18d68d5654938ec4e61e01d44727b3e2
Athan <jonpuray@gmail.com>
2025-02-19 02:27:03 +0800

    added requirements.txt

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def convert_toml_to_json(toml_file, json_file):
     except Exception as e:
         print(f"Error: {e}")
     
-    return json_file
+    return json_file                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 
 
 
@@ -29,13 +29,14 @@ if __name__ == '__main__':
     ]
 
     credentials = convert_toml_to_json(st.secrets, "credentials.json")
+    st.write(credentials.json)
 
     # creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
-    creds = Credentials.from_service_account_file(credentials, scopes=scopes)
-    client = gspread.authorize(creds)
+    # creds = Credentials.from_service_account_file(credentials, scopes=scopes)
+    # client = gspread.authorize(creds)
 
-    sheet_id = "1ZmilDNuV_h-w1OkKNwlbZCyD42KpaL5ilEK1hELRJpo"
-    sheet = client.open_by_key(sheet_id)
+    # sheet_id = "1ZmilDNuV_h-w1OkKNwlbZCyD42KpaL5ilEK1hELRJpo"
+    # sheet = client.open_by_key(sheet_id)
 
-    values_list = sheet.sheet1.row_values(1)
-    st.write(values_list)
+    # values_list = sheet.sheet1.row_values(1)
+    # st.write(values_list)
