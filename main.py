befdc800540bc5f62bf8ace11bb9eaa3673763d8
Athan <jonpuray@gmail.com>
2025-02-19 14:37:29 +0800

    edited main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def main():
 if __name__ == "__main__":
 
     with st.sidebar:
-        selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-        selected
+        selected = option_menu("Inventory Menu", ["Delivery", 'Set Out', 'Inventory'], 
+        icons=['truck', 'basket', 'bank'], menu_icon="cast", default_index=1)
+        
     main()
