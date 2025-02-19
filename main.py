ef731558e8f213b23afde217b5f9ece96de818c0
Athan <jonpuray@gmail.com>
2025-02-19 16:05:59 +0800

    edited main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def onhand():
     # df = df[1:]
     df.columns = ['Date', 'Item', 'Brand', 'Description', 'Quantity', 'Unit']
     df = df['Quantity'].astype(int)
-    new_df = df.groupby("Item", as_index=False).agg({"Quantity": "sum"})
+    new_df = df.groupby("Item")["Quantity"].sum()
 
     st.dataframe(new_df, use_container_width=True)
 
